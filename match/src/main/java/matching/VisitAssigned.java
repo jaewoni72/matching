208ab2010fb4dd256c0c5f19bdff63a98cecc1b1
김지연H님/NW Access EP151840C <P151840@sktelecom.com>
2021-01-19 13:39:47 +0900

    test

---
+++ match/src/main/java/matching/VisitAssigned.java
@@ -0,0 +1,14 @@
+package matching;
+
+public class VisitAssigned extends AbstractEvent {
+
+    private Long matchId;
+
+    public Long getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(Long matchId) {
+        this.matchId = matchId;
+    }
+}
